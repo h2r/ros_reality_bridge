5e97f7d0775500170f976a23937bf870e614ea70
David Whitney <davidfwhitney@gmail.com>
2017-10-05 13:14:53 -0400

    initialize repo

---
+++ src/lfdNode.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import String
+import tf
+from ein.msg import EinState
+
+def record_callback(data):
+	print hi
+
+def state_callback(data):
+	print type(data)
+
+def main():
+    # initialize the ROS node
+    rospy.init_node("lfdNode", anonymous=False)
+
+    # set up the subscriber
+    rospy.Subscriber("demonstrations", String, record_callback)
+    rospy.Subscriber("ein/right/state", String, state_callback)
+
+    # create a rate to sleep after every loop
+    rate = rospy.Rate(60)
+
+    # sleep to give time for publishers and listener to initilize
+    rospy.Rate(1).sleep()
+
+    while not rospy.is_shutdown():
+        rate.sleep()
+
+if __name__ == '__main__':
+    main()
