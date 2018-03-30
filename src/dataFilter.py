d4c0a3e089398f39e1634c95ca2071525cc631bb
Samir Gadre <syagadre@gmail.com>
2018-03-30 02:04:00 -0400

    multiskill pipeline backend working!

--- src/dataFilter.py
+++ src/dataFilter.py
@@ -26,21 +26,28 @@ class DataFilter(object):
 		z_fit, _ = optimize.curve_fit(self.f, t, z, guesses, sigma=sigma)
 		# p2, _ = optimize.curve_fit(self.f, x, y)
 
-		plt.figure(0)
-		plt.plot(t, x, "o")
-		plt.plot(t, self.f(t, *x_fit), "r", label=u"x data")
-		plt.show()
+		# plt.figure(0)
+		# plt.plot(t, x, "o")
+		# plt.plot(t, self.f(t, *x_fit), "r", label=u"x data")
+		# plt.show()
 
-		plt.figure(1)
-		plt.plot(t, y, "o")
-		plt.plot(t, self.f(t, *y_fit), "b", label=u"y data")
-		plt.show()
+		# plt.figure(1)
+		# plt.plot(t, y, "o")
+		# plt.plot(t, self.f(t, *y_fit), "b", label=u"y data")
+		# plt.show()
 
-		plt.figure(2)
-		plt.plot(t, z, "o")
-		plt.plot(t, self.f(t, *z_fit), "g", label=u"z data")
-		plt.show()
+		# plt.figure(2)
+		# plt.plot(t, z, "o")
+		# plt.plot(t, self.f(t, *z_fit), "g", label=u"z data")
+		# plt.show()
 		return np.array([self.f(t, *x_fit), self.f(t, *y_fit), self.f(t, *z_fit)]).T.tolist()
 
 	def f(self, t, *p):
 		return np.poly1d(p)(t)
+
+
+
+# points = np.array([[1.0, 3.0, 5.0], [1.2, 2.9, 5.2], [1.3, 2.8, 5.3], [1.4, 2.7, 5.4], [1.25, 2.76, 5.2], [1.0, 3.0, 5.0], [1.0, 2.93, 4.8], [1.5, 3.2, 4.8], [1.6, 3.3, 4.6]])
+# supports = np.array([0, 1, 3, 4, 8])
+# print DataFilter().filter(points, supports)
+
--- src/dmpNode.py
+++ src/dmpNode.py
@@ -11,11 +11,25 @@ from dataFilter import DataFilter
 
 class LFDHandler(object):
     def __init__(self):
+        # data for all the demonstrations
         self.traj_data = []
-        self.supports = [] # index of critical points in traj_data
-        self.plan = None
-        self.LFDRequest = None
+        self.supports = []
+
+        self.curr_skill_data = []
+        self.curr_skill_supports = [] # index of critical points in traj_data
+        
+        # start / end points for each skill (note end of i-th is start of i+1-th)
+        self.endpoints = []
+
+        # the lfd requests for all the skills
+        self.LFDRequests = []
+
+        # the current loc of the end effector
         self.curr_loc = None
+
+        self.pub = rospy.Publisher("/ein/right/forth_commands", String, queue_size = 0)
+
+
 
 
     #Learn a DMP from demonstration data
@@ -69,10 +83,23 @@ class LFDHandler(object):
         return resp;
 
 
-    def onEOF(self):
-        # rospy.loginfo("GOT EOF")
-        # rospy.loginfo("NUM DATA POINTS: " + str(len(self.traj_data)))
-        filtered_data = DataFilter().filter(np.array(self.traj_data), np.array(self.supports))
+    def moveToGlobalStart(self):
+        # code to linearly get to start position
+        curr_loc = self.curr_loc
+        num_steps = 20
+        start = self.endpoints[0]
+        x_vec = linspace(curr_loc[0], start[0], num=num_steps)
+        y_vec = linspace(curr_loc[1], start[1], num=num_steps)
+        z_vec = linspace(curr_loc[2], start[2], num=num_steps)
+        for i in xrange(len(x_vec)):
+            msg = str(x_vec[i]) + ' ' + str(y_vec[i]) + ' ' + str(z_vec[i]) + ' 0 1 0 0 moveToEEPose' 
+            self.pub.publish(msg)
+            rospy.sleep(0.2)
+
+        rospy.sleep(2)
+
+    def onEOS(self):
+        filtered_data = DataFilter().filter(np.array(self.curr_skill_data), np.array(self.curr_skill_supports))
         dims = 3                
         dt = 0.1 #1.0                
         K = 100                 
@@ -80,88 +107,81 @@ class LFDHandler(object):
         # num_bases = 4
         num_bases = len(self.supports)          
         # traj = [[1.0,1.0],[2.0,2.0],[3.0,4.0],[6.0,8.0]]
-        self.LFDRequest = self.makeLFDRequest(dims, filtered_data, dt, K, D, num_bases)
+        self.LFDRequests.append(self.makeLFDRequest(dims, filtered_data, dt, K, D, num_bases))
 
-    def onEXE(self, s, g):
+    def onEXE(self, skill_num):
         #Set it as the active DMP
-        if self.LFDRequest == None:
+        if self.LFDRequests == None or len(self.LFDRequests) == 0:
+            return
+        if skill_num >= len(self.LFDRequests) or skill_num < 0:
             return
 
-        self.makeSetActiveRequest(self.LFDRequest.dmp_list)
+        self.makeSetActiveRequest(self.LFDRequests[skill_num].dmp_list)
 
         #Now, generate a plan (with same start and end as demonstration)
-        x_0 = s#[0.0,0.0]          #Plan starting at a different point than demo 
+        x_0 = self.endpoints[skill_num]#[0.0,0.0]          #Plan starting at a different point than demo 
         x_dot_0 = [0.0,0.0,0.0]   
         t_0 = 0                
-        goal = g #[8.0,7.0]         #Plan to a different goal than demo
+        goal = self.endpoints[skill_num+1] #[8.0,7.0]         #Plan to a different goal than demo
         goal_thresh = [0.01, 0.01, 0.01]#[0.2,0.2,0.2]
         seg_length = -1          #Plan until convergence to goal
-        tau = self.LFDRequest.tau       #Desired plan should take as long as demo
+        tau = self.LFDRequests[skill_num].tau       #Desired plan should take as long as demo
         dt = 0.5
         integrate_iter = 1       #dt is rather large, so this is > 1  
-        plan = self.makePlanRequest(x_0, x_dot_0, t_0, goal, goal_thresh, 
+        plan_meta = self.makePlanRequest(x_0, x_dot_0, t_0, goal, goal_thresh, 
                                seg_length, tau, dt, integrate_iter)
-        if plan == None:
+        if plan_meta == None:
             return
 
         # save the parameterized motion plan
-        self.plan = plan.plan.points
+        plan = plan_meta.plan.points
         
-        if self.plan == None:
+        if plan == None:
             return
 
-
-        # case where we are going to publish the motion plan and move the robot
-        topic = "/ein/right/forth_commands"
-        pub = rospy.Publisher(topic, String, queue_size = 0)
-
-        # code to linearly get to start position
-        curr_loc = self.curr_loc
-        num_steps = 20
-        x_vec = linspace(curr_loc[0], s[0], num=num_steps)
-        y_vec = linspace(curr_loc[1], s[1], num=num_steps)
-        z_vec = linspace(curr_loc[2], s[2], num=num_steps)
-        for i in xrange(len(x_vec)):
-            msg = str(x_vec[i]) + ' ' + str(y_vec[i]) + ' ' + str(z_vec[i]) + ' 0 1 0 0 moveToEEPose' 
-            pub.publish(msg)
-            rospy.sleep(0.2)
-
-        rospy.sleep(2)
-
         # code to execute the motion plan
-        for dp in self.plan:
+        for dp in plan:
             point = dp.positions
             msg = str(point[0]) + ' ' + str(point[1]) + ' ' + str(point[2]) + ' 0 1 0 0 moveToEEPose' 
-            pub.publish(msg)
+            self.pub.publish(msg)
             rospy.sleep(0.2)
-
-        self.traj_data = []
 
     def callback(self, data):
         if data == None:
             return
-
         msg = data.data.split()
-        if msg[0] == "EOF":
-            self.onEOF()
-        elif msg[0] == "EXE":
+        if msg[0] == "EXE":
             # case where we want to execute a motion plan
+            data = msg[1:]
+            assert len(data) % 3 == 0 # x,y,z for each point
+            num_endpoints = len(data)/3
+            assert num_endpoints > 1
 
-            s = [float(msg[i]) for i in xrange(1, 4)]
-            g = [float(msg[i]) for i in xrange(4, 7)]
-            self.onEXE(s, g)
-        else:
+            for i in xrange(0, len(data), 3):
+                self.endpoints.append([float(data[j]) for j in xrange(i, i+3)])
+            self.moveToGlobalStart()
+            assert len(self.endpoints)-1 == len(self.traj_data)
+            for i in xrange(len(self.traj_data)):
+                self.onEXE(i) # sets active LFD and executes plan
+                rospy.sleep(2)
+        elif msg[0] == "EOS": # end of skill
+            if len(self.curr_skill_data) != 0:
+                self.traj_data.append(self.curr_skill_data)
+                self.supports.append(self.curr_skill_supports)
+                self.onEOS() # filters data and makes and LFDrequest
+                self.curr_skill_data = []
+                self.curr_skill_supports = []
+        elif msg[0] == "PT":
             # case where a point is getting sent
-            assert len(msg) == 4
-            self.curr_loc = [float(dim) for dim in msg[:-1]]
-            self.traj_data.append(self.curr_loc)
+            assert len(msg) == 5
+            self.curr_loc = [float(dim) for dim in msg[1:-1]]
+            self.curr_skill_data.append(self.curr_loc)
             if float(msg[-1]) == 1:
-                self.supports.append(len(self.traj_data) - 1)
+                self.curr_skill_supports.append(len(self.curr_skill_data) - 1)
         
 
     def start(self):
         rospy.init_node('dmp_bridge')
-
         # note dmp_train_data is created on the unity side as part of rosbridge
         rospy.Subscriber('dmp_train_data', String, self.callback)
         rospy.spin()
