87df2fc802dabb7d948e55ca41eb48a35dffd418
Gary Chien <garychien1@gmail.com>
2018-07-13 10:29:28 -0400

    reinitializes upon request by Unity

--- src/movocontrol_nav.py
+++ src/movocontrol_nav.py
@@ -1,16 +1,11 @@
-import roslib
-import rospy
-import math
-import tf
-import geometry_msgs.msg
+#!/usr/bin/env python
 import numpy as np
-from math import pow, atan2, sqrt
-from geometry_msgs.msg import Twist
-from sensor_msgs.msg import Joy
-from std_msgs.msg import String
-import sys
 import actionlib
+import rospy
+import tf
+from geometry_msgs.msg import Twist
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+from std_msgs.msg import String
 
 
 class Pose:
@@ -19,27 +14,26 @@ class Pose:
         self.y = y
         self.theta = theta
 
+
 class MovoTeleop:
     def __init__(self):
-        self.rate = rospy.Rate(10.0)
+        self.rate = rospy.Rate(10)
         self.pose = Pose()
         self.curr_state = 'standby'
-        listener = tf.TransformListener()
+        self.listener = tf.TransformListener()
+        self.ready_to_go = False
 
     def update_pose(self):
         while not rospy.is_shutdown():
             try:
-                (trans, rot) = listener.lookupTransform('/map', '/base_link', rospy.Time(0))
-                #print 'Trans:', trans
+                (trans, rot) = self.listener.lookupTransform('/map', '/base_link', rospy.Time(0))
                 z_rot = tf.transformations.euler_from_quaternion(rot)[2]
                 self.pose.x, self.pose.y = round(trans[0], 4), round(trans[1], 4)
                 self.pose.theta = np.rad2deg(z_rot)
                 self.rate.sleep()
-                #return (self.pose.x, self.pose.y, self.pose.theta)
                 return
             except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
                 continue
-    
 
     def move2goal(self, goal_x, goal_y, goal_theta):
         assert self.curr_state == 'navigating'
@@ -51,6 +45,7 @@ class MovoTeleop:
         except rospy.ROSInterruptException:
             rospy.loginfo('Navigation test finished.')
 
+
 def movebase_client(goal_x, goal_y, goal_theta):
     client = actionlib.SimpleActionClient('movo_move_base', MoveBaseAction)
     client.wait_for_server()
@@ -61,9 +56,6 @@ def movebase_client(goal_x, goal_y, goal_theta):
     goal.target_pose.pose.position.x = goal_x
     goal.target_pose.pose.position.y = goal_y
 
-    # TODO: input goal_theta as target orientation
-    # -------------------------------------------------------------
-    #goal.target_pose.pose.orientation.w = 1.0
     print 'GOAL:', goal_theta
     yaw = np.deg2rad(goal_theta)
     quaternion = tf.transformations.quaternion_from_euler(0, 0, yaw)
@@ -72,8 +64,6 @@ def movebase_client(goal_x, goal_y, goal_theta):
     goal.target_pose.pose.orientation.y = quaternion[1]
     goal.target_pose.pose.orientation.z = quaternion[2]
     goal.target_pose.pose.orientation.w = quaternion[3]
-    #goal.target_pose.pose.orientation.y = np.deg2rad(goal_theta) # TODO: check if this works.
-    # -------------------------------------------------------------
 
     client.send_goal(goal)
     wait = client.wait_for_result()
@@ -83,25 +73,13 @@ def movebase_client(goal_x, goal_y, goal_theta):
     else:
         return client.get_result()
 
-def unit(v):
-    v = np.asarray(v)
-    return v / np.linalg.norm(v)
-
-def del_angle(v1, v2):
-    v1 = np.asarray(v1)
-    v2 = np.asarray(v2)
-    u1 = v1 / np.linalg.norm(v1)
-    u2 = v2 / np.linalg.norm(v2)
-    s = np.linalg.norm(np.cross(v1, v2))
-    c = np.dot(v1, v2)
-    return np.arctan2(s, c)
 
 def waypoint_callback(data):
     print 'waypoint callback:', data.data
     pose = data.data.split(';')
     print 'pose:', pose
     movo.curr_state = 'navigating'
-    print 'set state to navigating'
+    # print 'set state to navigating'
     for p in pose:
         pose_data = p.split(',')
         assert len(pose_data) == 3
@@ -109,45 +87,41 @@ def waypoint_callback(data):
         goal_y = float(pose_data[1])
         goal_theta = float(pose_data[2])
         movo.move2goal(goal_x, goal_y, goal_theta)
-    #coords = data.data.split(';')
-    #print 'coords:', coords
-    #movo.curr_state = 'navigating'
-    #print 'set state to navigating'
-    #for coord in coords:
-    #    coord = coord.split(',')
-    #    assert len(coord) == 2
-    #    goal_x = float(coord[0])
-    #    goal_y = float(coord[1])
-    #    movo.move2goal(goal_x, goal_y)
-    print 'set state to standby'
+    # print 'set state to standby'
     movo.curr_state = 'standby'
 
+
 def state_request_callback(data):
-    print 'state_request_callback:', movo.curr_state
+    if not movo.ready_to_go:
+        print 'Movocontrol is initialized and ready to go!'
+        movo.ready_to_go = True
     assert (movo.curr_state in ['standby', 'navigating'])
     movo_state_publisher.publish(movo.curr_state)
 
+
 def poserequest_callback(data):
-    print 'poserequest_callback'
+    # print 'poserequest_callback'
     movo.update_pose()
     movo_pose_publisher.publish('{},{},{}'.format(movo.pose.x, movo.pose.y, movo.pose.theta))
+
+
+def initialize_request_callback(data):
+    print 'resetting movocontrol_nav...'
+    movo.ready_to_go = False
+
 
 if __name__ == '__main__':
     try:
         listener = tf.TransformListener()
-        #max_speed = 0.2
-        #min_speed = 0.1
-        #rotation_tolerance = 0.05
-        #distance_tolerance = 0.15
         rospy.init_node('movo_controller', anonymous=True)
+        movo = MovoTeleop()
         velocity_publisher = rospy.Publisher('/movo/cmd_vel', Twist, queue_size=10)
         movo_state_publisher = rospy.Publisher('holocontrol/ros_movo_state_pub', String, queue_size=0)
         movo_pose_publisher = rospy.Publisher('holocontrol/ros_movo_pose_pub', String, queue_size=0)
         rospy.Subscriber('holocontrol/movo_state_request', String, state_request_callback)
         rospy.Subscriber('holocontrol/movo_pose_request', String, poserequest_callback)
         rospy.Subscriber('holocontrol/unity_waypoint_pub', String, waypoint_callback)
-        movo = MovoTeleop()
-        movo.curr_state = 'standby'
+        rospy.Subscriber('holocontrol/init_movocontrol_request', String, initialize_request_callback)
     except (rospy.ROSInterruptException, KeyboardInterrupt):
         print 'ROS exception :('
     rospy.spin()
